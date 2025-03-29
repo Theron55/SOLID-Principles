35af71ad51e2626195c6d23dc026832bf97362c7
Theron Hamlin <91212081+Theron55@users.noreply.github.com>
2025-03-28 19:17:38 -0700

    Update SRPViolation.java

--- 1_SingleResponsibility/SRPViolation.java
+++ 1_SingleResponsibility/SRPViolation.java
@@ -1,1 +1,25 @@
-hi
+package SRP;
+
+public class SRPViolation {
+    public static void main(String[] args) {
+        Report report = new Report("This is a test report.");
+        report.printReport();
+        report.saveToFile("report.txt");
+    }
+}
+
+class Report {
+    private String content;
+
+    public Report(String content) {
+        this.content = content;
+    }
+
+    public void printReport() {
+        System.out.println("Printing report: " + content);
+    }
+
+    public void saveToFile(String filename) {
+        System.out.println("Saving report to file: " + filename);
+    }
+}
