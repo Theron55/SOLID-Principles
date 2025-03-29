85bc70f4ae0c0aa6dbe1c544dee65e7a2a09d81b
Theron Hamlin <91212081+Theron55@users.noreply.github.com>
2025-03-28 19:19:13 -0700

    Update LSPViolation.java

--- 3_LiskovSubstitution/LSPViolation.java
+++ 3_LiskovSubstitution/LSPViolation.java
@@ -1,1 +1,26 @@
+package LSP;
+
+public class LSPViolation {
+    public static void main(String[] args) {
+        Bird bird = new Ostrich();
+        makeBirdFly(bird);  // 💥 This will throw an exception!
+    }
+
+    public static void makeBirdFly(Bird bird) {
+        bird.fly();
+    }
+}
+
+class Bird {
+    public void fly() {
+        System.out.println("Flying...");
+    }
+}
+
+class Ostrich extends Bird {
+    @Override
+    public void fly() {
+        throw new UnsupportedOperationException("Ostriches can't fly!");
+    }
+}
 
