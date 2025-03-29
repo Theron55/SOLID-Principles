2ca887068abc0c228cb7a89e9dd64d8717b23685
Theron Hamlin <91212081+Theron55@users.noreply.github.com>
2025-03-28 19:20:00 -0700

    Update DIPViolation.java

--- 4_DependencyInversion/DIPViolation.java
+++ 4_DependencyInversion/DIPViolation.java
@@ -1,1 +1,23 @@
+package DIP;
+
+public class DIPViolation {
+    public static void main(String[] args) {
+        App app = new App();
+        app.store("User data");
+    }
+}
+
+class MySQLDatabase {
+    public void save(String data) {
+        System.out.println("Saving '" + data + "' to MySQL database.");
+    }
+}
+
+class App {
+    private MySQLDatabase db = new MySQLDatabase(); // Direct dependency on concrete class
+
+    public void store(String data) {
+        db.save(data);
+    }
+}
 
