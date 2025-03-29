2e98717a25558254df40b42818514324ea457c26
Theron Hamlin <91212081+Theron55@users.noreply.github.com>
2025-03-28 19:20:39 -0700

    Update ISPViolation.java

--- 5_InterfaceSegregation/ISPViolation.java
+++ 5_InterfaceSegregation/ISPViolation.java
@@ -1,1 +1,35 @@
+package ISP;
+
+public class ISPViolation {
+    public static void main(String[] args) {
+        Worker human = new HumanWorker();
+        Worker robot = new RobotWorker();
+
+        human.work();
+        human.eat();
+
+        robot.work();
+        robot.eat();  // ⚠️ This makes no sense for a robot!
+    }
+}
+
+interface Worker {
+    void work();
+    void eat();
+}
+
+class HumanWorker implements Worker {
+    public void work() {
+        System.out.println("Human is working.");
+    }
+
+    public void eat() {
+        System.out.println("Human is eating lunch.");
+    }
+}
+
+class RobotWorker implements Worker {
+    public void work() {
+        System.out.println("Robot is working.");
+    }
 
