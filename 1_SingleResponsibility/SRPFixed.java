5c7292d92c12281413a169cedf9f1ba5bae4ac5e
Theron Hamlin <91212081+Theron55@users.noreply.github.com>
2025-03-28 19:17:52 -0700

    Update SRPFixed.java

--- 1_SingleResponsibility/SRPFixed.java
+++ 1_SingleResponsibility/SRPFixed.java
@@ -1,1 +1,38 @@
+package SRP;
+
+public class SRPFixed {
+    public static void main(String[] args) {
+        Report report = new Report("This is a test report.");
+        ReportPrinter printer = new ReportPrinter();
+        ReportSaver saver = new ReportSaver();
+
+        printer.print(report);
+        saver.saveToFile(report, "report.txt");
+    }
+}
+
+class Report {
+    private String content;
+
+    public Report(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
+
+class ReportPrinter {
+    public void print(Report report) {
+        System.out.println("Printing report: " + report.getContent());
+    }
+}
+
+class ReportSaver {
+    public void saveToFile(Report report, String filename) {
+        System.out.println("Saving report to file: " + filename);
+        // Imagine file-saving logic here
+    }
+}
 
