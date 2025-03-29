59f646eb31a6f0f784022317bc41bd034bd19e92
Theron Hamlin <91212081+Theron55@users.noreply.github.com>
2025-03-28 19:20:15 -0700

    Update DIPFixed.java

--- 4_DependencyInversion/DIPFixed.java
+++ 4_DependencyInversion/DIPFixed.java
@@ -1,1 +1,32 @@
+package DIP;
+
+public class DIPFixed {
+    public static void main(String[] args) {
+        Database db = new MySQLDatabase(); // Inject dependency via abstraction
+        App app = new App(db);
+        app.store("User data");
+    }
+}
+
+interface Database {
+    void save(String data);
+}
+
+class MySQLDatabase implements Database {
+    public void save(String data) {
+        System.out.println("Saving '" + data + "' to MySQL database.");
+    }
+}
+
+class App {
+    private Database db;
+
+    public App(Database db) {
+        this.db = db;
+    }
+
+    public void store(String data) {
+        db.save(data);
+    }
+}
 
